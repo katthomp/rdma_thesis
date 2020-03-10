01f7687aaa6546a8298c85790d6ab02fa627f2ac
katthomp <katjthomp@reed.edu>
2020-03-09 22:20:11 -0500

    unsure if post_send and post_receive are in the right places

--- mini_rdma_client.c
+++ mini_rdma_client.c
@@ -27,3 +27,56 @@ static int resources_create_client(struct resources *res){
         }
 
 }
+
+
+//client will only be sending wr, correct?
+//TODO
+static int
+post_send (struct resources *res, int opcode)
+{  
+ struct ibv_sge sge;
+    struct ibv_send_wr *bad_wr = NULL;
+    int rc;
+    /* prepare the scatter/gather entry */
+    memset (&sge, 0, sizeof (sge));
+    sge.addr = (uintptr_t) res->buf;
+    sge.length = MSG_SIZE;
+    sge.lkey = res->mr->lkey;
+    /* prepare the send work request */
+    memset (&sr, 0, sizeof (sr));
+    sr.next = NULL;
+    sr.wr_id = 0;
+    sr.sg_list = &sge;
+    sr.num_sge = 1;
+    sr.opcode = opcode;
+    sr.send_flags = IBV_SEND_SIGNALED;
+    if (opcode != IBV_WR_SEND)
+    {   
+        sr.wr.rdma.remote_addr = res->remote_props.addr;
+        sr.wr.rdma.rkey = res->remote_props.rkey;
+    }
+    /* there is a Receive Request in the responder side, so we won't get any into RNR flow */ 
+    rc = ibv_post_send (res->qp, &sr, &bad_wr);
+    if (rc)
+        fprintf (stderr, "failed to post SR\n");
+    else
+    {   
+        switch (opcode)
+        {   
+            case IBV_WR_SEND:
+                fprintf (stdout, "Send Request was posted\n");
+                break;
+            case IBV_WR_RDMA_READ:
+                fprintf (stdout, "RDMA Read Request was posted\n");
+                break;
+            case IBV_WR_RDMA_WRITE:
+                fprintf (stdout, "RDMA Write Request was posted\n");
+                break;
+            default:
+                fprintf (stdout, "Unknown Request was posted\n");
+                break;
+        }
+    }
+    return rc;
+}
+
--- mini_rdma_server.c
+++ mini_rdma_server.c
@@ -34,6 +34,89 @@ int resources_create_server(struct resources *res){
             close_resources(res, rc); //link up with the def in mini_rdma_cmn.c
         }
 
-
 }
+
+//pretty sure that poll_completion should go into here
+//only because the server is always going to be running this function, right?
+//TODO
+static int
+poll_completion (struct resources *res)
+{   
+    struct ibv_wc wc;
+    unsigned long start_time_msec;
+    unsigned long cur_time_msec;
+    struct timeval cur_time;
+    int poll_result;
+    int rc = 0;
+    /* poll the completion for a while before giving up of doing it .. */
+    gettimeofday (&cur_time, NULL);
+    start_time_msec = (cur_time.tv_sec * 1000) + (cur_time.tv_usec / 1000);
+    do
+    {   
+        poll_result = ibv_poll_cq (res->cq, 1, &wc);
+        gettimeofday (&cur_time, NULL);
+        cur_time_msec = (cur_time.tv_sec * 1000) + (cur_time.tv_usec / 1000);
+    }
+    while ((poll_result == 0)
+            && ((cur_time_msec - start_time_msec) < MAX_POLL_CQ_TIMEOUT));
+    if (poll_result < 0)
+    {   
+        /* poll CQ failed */
+        fprintf (stderr, "poll CQ failed\n");
+        rc = 1;
+    }
+    else if (poll_result == 0)
+    {   
+        /* the CQ is empty */
+        fprintf (stderr, "completion wasn't found in the CQ after timeout\n");
+        rc = 1;
+    }
+    else
+    {   
+        /* CQE found */
+        fprintf (stdout, "completion was found in CQ with status 0x%x\n",
+                wc.status);
+        /* check the completion status (here we don't care about the completion opcode */
+        if (wc.status != IBV_WC_SUCCESS)
+        {   
+            fprintf (stderr,
+                    "got bad completion with status: 0x%x, vendor syndrome: 0x%x\n",
+                    wc.status, wc.vendor_err);
+            rc = 1;
+        }
+    }
+    return rc;
+}
+
+
+//server will only be receiving.
+//TODO
+static int
+post_receive (struct resources *res)
+{
+    struct ibv_recv_wr rr;
+    struct ibv_sge sge;
+    struct ibv_recv_wr *bad_wr;
+    int rc;
+    /* prepare the scatter/gather entry */
+    memset (&sge, 0, sizeof (sge));
+    sge.addr = (uintptr_t) res->buf;
+    sge.length = MSG_SIZE;
+    sge.lkey = res->mr->lkey;
+    /* prepare the receive work request */
+    memset (&rr, 0, sizeof (rr));
+    rr.next = NULL;
+    rr.wr_id = 0;
+    rr.sg_list = &sge;
+    rr.num_sge = 1;
+    /* post the Receive Request to the RQ */
+    rc = ibv_post_recv (res->qp, &rr, &bad_wr);
+    if (rc)
+        fprintf (stderr, "failed to post RR\n");
+    else
+        fprintf (stdout, "Receive Request was posted\n");
+    return rc;
+}
+
+
 
